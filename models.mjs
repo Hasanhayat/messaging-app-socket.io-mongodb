cbf0ea4b5d35be8a7b9c820569bfd0334dba57a0
hasanhayat <aylskhsmvz.v@gmail.com>
2025-07-17 20:17:55 +0500

    run

---
+++ models.mjs
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdOn: { type: Date, default: Date.now },
+});
+
+export const User = mongoose.model("User", userSchema);
+
+
+const messageSchema = new mongoose.Schema({
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  content: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
+  image: { type: String, default: null },
+});
+export const Message = mongoose.model("Message", messageSchema);

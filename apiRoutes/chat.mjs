df2853e814ae6cc61959ad6d4f73fc3e170715dd
hasanhayat <aylskhsmvz.v@gmail.com>
2025-07-17 21:06:35 +0500

    messages in progress

---
+++ apiRoutes/chat.mjs
@@ -0,0 +1,65 @@
+import express from 'express';
+import http from 'http';    
+import { Message, User } from '../models.mjs';
+
+
+const router = express.Router();
+
+
+router.get("/profile", async (req, res) => {
+  const user = req.user;
+  try {
+    const user = req.user;
+    res.send({ message: "User profile", user: user });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+
+
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({}, '-password -__v');
+    res.json({ message: "Users fetched successfully", users: users });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.get("/user/:id", async (req, res) => {
+    const userId = req.params.id;
+    try {
+        const user = await User.findById(userId, '-password -__v');
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        res.json({ message: "User fetched successfully", user: user });
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+);
+
+router.post("message", async (req, res) => {
+  const { senderId, receiverId, content } = req.body;
+    try {
+        const message = Message.create({sender: senderId, receiver: receiverId, content: content});
+        if (!message) {
+            return res.status(400).json({ error: "Failed to send message" });
+        }
+        res.status(201).json({ message: "Message sent successfully", message: message });
+    } catch (error) {
+        console.error("Error sending message:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+);
+
+
+export default router;
+
+
+
--- server.mjs
+++ server.mjs
@@ -4,7 +4,9 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import mongoose from 'mongoose';
 import 'dotenv/config'
+import jwt from 'jsonwebtoken';
 import auth from './apiRoutes/auth.mjs'
+import chat from './apiRoutes/chat.mjs'
 
 const app = express();
 
@@ -18,7 +20,7 @@ app.use(
 );
 
 const PORT = process.env.PORT || 8080;
-const JWT_SECRET = process.env.SECRET_TOKEN;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 
 mongoose.connect(process.env.MONGO_URI);
@@ -33,8 +35,6 @@ app.get("/api/v1/", (req, res) => {
 });
 app.use("/api/v1", auth)
 
-
-// Middleware to check JWT token
 app.use("/api/v1/*splat", (req, res, next) => {
   const token = req.cookies.Token;
   if (!token) {
@@ -49,100 +49,13 @@ app.use("/api/v1/*splat", (req, res, next) => {
   });
 });
 
-app.get("/api/v1/profile", async (req, res) => {
-  const user = req.user;
-  try {
-    let result = await db.query("SELECT * FROM users WHERE id = $1", [user.id]);
-    res.send({ message: "User profile", user: result.rows[0] });
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
+app.use("/api/v1", chat)
 
 
 
-app.get("/api/v1/products", async (req, res) => {
-  try {
-    const products = await db.query(
-      "SELECT name, description, price, image, category_name FROM products INNER JOIN categories ON products.category_id = categories.category_id"
-    );
-    res.json(products.rows);
-  } catch (error) {
-    console.error("Error fetching products:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
 
-app.get("/api/v1/categories", async (req, res) => {
-  try {
-    const categories = await db.query("SELECT * FROM categories");
-    res.json(categories.rows);
-  } catch (error) {
-    console.error("Error fetching categories:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
 
-//middeware to check if user is admin
-app.use("/api/v1/*splat", (req, res, next) => {
-  const user = req.user;
-  if (user.user_role !== 1) {
-    return res.status(403).json({ error: "Forbidden: Admins only" });
-  }
-  next();
-});
-
-app.post("/api/v1/categories", async (req, res) => {
-  const { name } = req.body;
-  try {
-    if (!name) {
-      return res.status(400).json({ error: "All fields are required" });
-    }
-    const newCategory = await db.query(
-      "INSERT INTO categories (category_name) VALUES ($1) RETURNING *",
-      [name]
-    );
-    res.status(201).json(newCategory.rows[0]);
-  } catch (error) {
-    console.error("Error adding category:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-app.post("/api/v1/products", async (req, res) => {
-  const { name, description, price, image, category_id } = req.body;
-  try {
-    if (!name || !description || !price || !image || !category_id) {
-      return res.status(400).json({ error: "All fields are required" });
-    }
-
-    const newProduct = await db.query(
-      "INSERT INTO products (name, description, price, image, category_id) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-      [name, description, price, image, category_id]
-    );
-
-    res.status(201).json(newProduct.rows[0]);
-  } catch (error) {
-    console.error("Error adding product:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-app.get("/api/v1/users", async (req, res) => {
-  try {
-    const users = await db.query(
-      "SELECT id, first_name, last_name, email, profile_img, role, phone, created_at FROM users"
-    );
-    res.json({ message: "Users fetched successfully", users: users.rows });
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-// Middleware to serve static files
 let __dirname = path.resolve();
-// app.use(express.static(path.join(__dirname, "frontend", "dist")));
 app.use("/", express.static(path.join(__dirname, "./FRONTEND/dist")));
 app.use("/*splat", express.static(path.join(__dirname, "FRONTEND", "dist")));
 

c4ea9321f836e3d915e23fc120a4c8f5fa716f63
hasanhayat <aylskhsmvz.v@gmail.com>
2025-07-17 21:57:23 +0500

    message done

--- apiRoutes/chat.mjs
+++ apiRoutes/chat.mjs
@@ -1,10 +1,8 @@
-import express from 'express';
-import http from 'http';    
-import { Message, User } from '../models.mjs';
-
+import express from "express";
+import http from "http";
+import { Message, User } from "../models.mjs";
 
 const router = express.Router();
-
 
 router.get("/profile", async (req, res) => {
   const user = req.user;
@@ -16,11 +14,9 @@ router.get("/profile", async (req, res) => {
   }
 });
 
-
-
 router.get("/users", async (req, res) => {
   try {
-    const users = await User.find({}, '-password -__v');
+    const users = await User.find({}, "-password -__v");
     res.json({ message: "Users fetched successfully", users: users });
   } catch (error) {
     console.error("Error fetching users:", error);
@@ -29,37 +25,56 @@ router.get("/users", async (req, res) => {
 });
 
 router.get("/user/:id", async (req, res) => {
-    const userId = req.params.id;
-    try {
-        const user = await User.findById(userId, '-password -__v');
-        if (!user) {
-            return res.status(404).json({ error: "User not found" });
-        }
-        res.json({ message: "User fetched successfully", user: user });
-    } catch (error) {
-        console.error("Error fetching user:", error);
-        res.status(500).json({ error: "Internal server error" });
+  const userId = req.params.id;
+  try {
+    const user = await User.findById(userId, "-password -__v");
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
-}
-);
+    res.json({ message: "User fetched successfully", user: user });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 router.post("message", async (req, res) => {
   const { senderId, receiverId, content } = req.body;
-    try {
-        const message = Message.create({sender: senderId, receiver: receiverId, content: content});
-        if (!message) {
-            return res.status(400).json({ error: "Failed to send message" });
-        }
-        res.status(201).json({ message: "Message sent successfully", message: message });
-    } catch (error) {
-        console.error("Error sending message:", error);
-        res.status(500).json({ error: "Internal server error" });
+  try {
+    const message = Message.create({
+      sender: senderId,
+      receiver: receiverId,
+      content: content,
+    });
+    if (!message) {
+      return res.status(400).json({ error: "Failed to send message" });
     }
-}
-);
-
-
+    res
+      .status(201)
+      .json({ message: "Message sent successfully", message: message });
+  } catch (error) {
+    console.error("Error sending message:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+router.get("messages", async (req, res) => {
+  const { senderId, receiverId } = req.body;
+   try {
+        let conversation = await messageModel.find({
+            $or: [
+                {
+                    from: receiverId,
+                    to: senderId
+                },
+                {
+                    from: senderId,
+                    to: receiverId,
+                }
+            ]
+        })
+        res.send({message: "Message Found", conversation: conversation})
+    } catch (error) {
+        res.status(500).send({message: "Internal Server Error"})
+    }
+});
 export default router;
-
-
-
